2cc77a30fbecf060c10dc1b4eae2732c38b2dba3
vkwho9ar <vkwho9ar@gmail.com>
2021-04-28 15:11:43 -0400

    started on practice page

--- landing-page-clone/src/App.js
+++ landing-page-clone/src/App.js
@@ -1,10 +1,12 @@
 import "./styles/css/styles.css";
 import Home from "./views/Home";
+import Practice from "./views/Practice";
 
 function App() {
    return (
       <div className="App">
          <Home />
+         <Practice />
       </div>
    );
 }
---
+++ landing-page-clone/src/views/Practice.js
@@ -0,0 +1,14 @@
+function Practice() {
+   return (
+      <div id="practice-page" className="page-view">
+         <div className="main-container practice">
+            <div className="heading">
+               <h2>What</h2>
+               <h2>We Do</h2>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Practice;
